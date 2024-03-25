60bd543233d7d4c54612390923855aaabefdd624
Ivan <ivan89x@hotmail.com>
2024-03-25 12:54:07 -0300

    functions ok

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,37 +1,38 @@
 import '../modules/toggle-mode.js';
 import timer from '../modules/timer.js';
-import { buttonPressAudio, kitchenTimer , bgAudio } from "./sounds.js";
+
+//import { buttonPressAudio, kitchenTimer , bgAudio } from "../modules/sounds.js";
+// essa estratégia de import de audios causou um erro no Firefox: "NotSupportedError: The media resource indicated by the src attribute or assigned media provider object was not suitable"
+// então eu adicionei audio tags manualmente na página html e instanciei aqui:
+const bgAudio = document.getElementById("bg-audio");
+const buttonPressAudio = document.getElementById("button-press");
+const kitchenTimer = document.getElementById("kitchen-timer");
 
 const controls_arr = Array.prototype.slice.call(document.getElementById("controls").children);
-// document.getElementById("controls").children returns an HTML collection, not an array
-// Array.prototype.slice.call() converts it to an array so it can be iterated
+// document.getElementById("controls").children retorna uma HTML Collection, não um array
+// Array.prototype.slice.call() converte para um array que pode ser iterado:
 
 controls_arr.forEach(element => {
     element.addEventListener('click',(event)=>{
+        const doc_classes = document.documentElement.classList;
         switch(event.target.id){
             case "musicon":
                 bgAudio.play();
+                doc_classes.toggle("silence");
                 break;
             case "musicoff":
                 bgAudio.pause();
+                doc_classes.toggle("silence");
                 break;
+            case "stop":
+                kitchenTimer.play();
             default:
-                const doc_classes = document.documentElement.classList;
                 timer[event.target.id]();
                 if((timer.isrunning && !doc_classes.contains('running')) || (!timer.isrunning && doc_classes.contains('running'))){
                     doc_classes.toggle('running');
                     doc_classes.toggle('paused');
                     buttonPressAudio.play()
-                };
-                if(timer.miliseconds == 0){
-                    kitchenTimer.play()
-                };
-                break
-        }
-        
+                }
+        }        
     })
 });
-
-/* setInterval(() => {
-    console.log(timer.minutes, timer.seconds, timer.miliseconds)
-}, 1243); */
